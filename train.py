654eecd7d3cc756e96a83ab23b1fc4337f332485
knapsack96 <30894109+knapsack96@users.noreply.github.com>
2022-06-28 18:57:10 +0200

    Update train.py

--- train.py
+++ train.py
@@ -182,25 +182,6 @@ def train(args):
 
             logger.push(metrics)
 
-            if total_steps % VAL_FREQ == VAL_FREQ - 1:
-                PATH = 'checkpoints/%d_%s.pth' % (total_steps+1, args.name)
-                torch.save(model.state_dict(), PATH)
-
-                results = {}
-                for val_dataset in args.validation:
-                    if val_dataset == 'chairs':
-                        results.update(evaluate.validate_chairs(model.module))
-                    elif val_dataset == 'sintel':
-                        results.update(evaluate.validate_sintel(model.module))
-                    elif val_dataset == 'kitti':
-                        results.update(evaluate.validate_kitti(model.module))
-
-                logger.write_dict(results)
-                
-                model.train()
-                if args.stage != 'chairs':
-                    model.module.freeze_bn()
-            
             total_steps += 1
 
             if total_steps > args.num_steps:
